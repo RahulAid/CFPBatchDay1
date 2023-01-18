681862f2832a8615c81e7e221fed4d96e5adbe80
RahulAid <raccess97@gmail.com>
2023-01-18 13:19:09 +0530

    Let Var Const and Hoisting

---
+++ Hoisting.js
@@ -0,0 +1,5 @@
+console.log(a)   //Declaration Hoisted to the top but not Initialization and remains undefined
+                 // Only works with Var. Doesn't work with let and const
+var a=9
+//let a = 8
+//const a = 9
---
+++ LetVarConst.js
@@ -0,0 +1,38 @@
+//function Let()
+//{
+  //  let a = 12; //Used for declaration & initialization and cannot be repeated in the function
+  //  a= 13;
+  //  console.log(a);
+//}
+//Let();
+
+//function Var()
+//{
+    //var a = 12   //Used for declaration and initialization and can be repeated
+    //var a = 13
+    //console.log(a)
+//}
+//Var();
+
+//function Const()
+//{
+  //  const a = 12
+  //  a = 13
+  //  console.log(a)
+//}
+//Const();
+
+///function Constant()
+//{
+    //const a = {Fname : 'Rahul'};  //Used for declaration & initialization  
+    //a.Fname = 'Kumar';            //Can be used to change properties but not values
+    //a.Lname = 'Rahul'
+    //console.log(a.Fname,a.Lname); 
+ //}
+ //Constant();
+
+//Arrow Function
+
+let a = (b,c) => console.log(b+c);
+ a(2,3);
+ 
